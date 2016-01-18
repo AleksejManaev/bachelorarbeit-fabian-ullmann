e04e569f2ae9bf3220a0743c03fdbe5f1498e01d
mlevental <m.levental@web.de>
2016-01-18 19:17:33 +0100

    Praktikumsansicht-Tutor: Zustand der Tabelle wird während der Session gespeichert

--- static/js/tutor_index.js
+++ static/js/tutor_index.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    <!-- Jede Spalte der Tabelle mit einem nicht-leeren th-Element innerhalb von tfoot erhält eine Searchbox -->
     $('#placements-table tfoot th').each(function () {
         if ($(this).html()) {
             var title = $(this).text();
@@ -6,12 +7,13 @@ $(document).ready(function () {
         }
     });
 
+    <!-- Tabelleninitialisierung -->
     var table = $('#placements-table').DataTable({
         "columnDefs": [
             {"searchable": false, "orderable": false, "targets": [0, 1, 10]},
             {"orderDataType": "dom-text", "targets": 6},
             {"orderDataType": "dom-checkbox", "targets": [7, 9]},
-            {"orderDataType": "dom-select", "targets": 8},
+            {"orderDataType": "dom-select", "targets": 8}
         ],
         "order": [[8, "desc"]],
         "dom": 't<"row"<"col-sm-6 col-sm-offset-1"p>>',
@@ -21,9 +23,25 @@ $(document).ready(function () {
                 "previous": "Zurück",
                 "next": "Weiter"
             }
-        }
+        },
+        stateSave: true
     });
 
+    <!-- Zustand der Searchboxen wiederherstellen -->
+    var state = table.state.loaded();
+    if (state) {
+        table.columns().eq(0).each(function (colIdx) {
+            var colSearch = state.columns[colIdx].search;
+
+            if (colSearch.search) {
+                $('input', table.column(colIdx).footer()).val(colSearch.search);
+            }
+        });
+
+        table.draw();
+    }
+
+    <!-- Suchfunktion für jede Searchbox -->
     table.columns().every(function () {
         var that = this;
 
