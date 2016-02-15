64b9e19e7b20cd5eddb45b72c77010ef78425fc1
mlevental <someuser@mail.com>
2016-02-15 18:52:31 +0100

    Ungenutze Formulare entfernt

--- mentoring/forms.py
+++ mentoring/forms.py
@@ -1,35 +1,12 @@
 # -*- coding: utf-8 -*-
 from django import forms
 from django.forms.utils import ErrorList
-from mentoring.models import *
-from django.utils.translation import ugettext_lazy as _
-from mentoring.widgets import ClearableFileInput
 from django.forms.widgets import DateInput
 
+from mentoring.models import *
+from mentoring.widgets import ClearableFileInput
+
 __author__ = 'ullmanfa'
-
-
-# Todo TestForm kann gelöscht werden
-class TestForm(forms.Form):
-    subject = forms.CharField(widget=forms.TextInput(attrs={'length': '100'}), label="Hallo Welt", max_length=100)
-
-    disabled = forms.CharField(label="Hallo Welt", max_length=100,
-                               widget=forms.TextInput(attrs={'disabled': 'disabled'}))
-    textfield = forms.CharField()
-    message = forms.CharField(widget=forms.Textarea)
-    sender = forms.EmailField(widget=forms.EmailInput(attrs={'icon': 'editor-mode-edit'}))
-    password = forms.CharField(widget=forms.PasswordInput)
-    file = forms.FileField()
-    CHOICES = (('1', 'First',), ('2', 'Second',))
-    choice_field = forms.ChoiceField(widget=forms.Select, choices=CHOICES)
-    radio_select = forms.ChoiceField(widget=forms.RadioSelect, choices=CHOICES)
-    checkboxes = forms.ChoiceField(widget=forms.CheckboxSelectMultiple, choices=CHOICES)
-    checkboxes_input = forms.ChoiceField(widget=forms.CheckboxInput)
-    switch = forms.ChoiceField(widget=forms.CheckboxInput(attrs={'switch': 'switch', 'on': 'an', 'off': 'aus'}),
-                               choices=CHOICES)
-    range = forms.IntegerField(widget=forms.NumberInput(attrs={'type': 'range', 'min': '0', 'max': '100'}))
-    integer = forms.IntegerField()
-    date = forms.DateField()
 
 
 class FormCompany(forms.ModelForm):
@@ -57,18 +34,6 @@ class FormCompany(forms.ModelForm):
 
     class Meta:
         model = Company
-        fields = '__all__'
-
-
-class FormContactData(forms.ModelForm):
-    class Meta:
-        model = ContactData
-        fields = ['title', 'first_name', 'last_name', 'email', 'phone']
-
-
-class FormTutor2ContactData(forms.ModelForm):
-    class Meta:
-        model = Tutor2ContactData
         fields = '__all__'
 
 
@@ -137,67 +102,6 @@ class FormTutorPlacement(forms.ModelForm):
         fields = ['number_seminars_present', 'presentation_done', 'placement_completed', 'mentoring_accepted']
 
 
-class FormPlacementEventRegistration(forms.ModelForm):
-    def __init__(self, *args, **kwargs):
-        super(FormPlacementEventRegistration, self).__init__(*args, **kwargs)
-        self.fields['event'].queryset = self.fields['event'].queryset.filter(course=self.instance.placement.course)
-
-    class Meta:
-        model = PlacementEventRegistration
-        fields = ['event']
-        widgets = {
-            'event': forms.RadioSelect(),
-        }
-
-
-class FormEvent(forms.ModelForm):
-    class Meta:
-        model = PlacementEvent
-        fields = ['date', 'time', 'room']
-
-
-class FormThesisDocuments(forms.ModelForm):
-    def is_valid(self):
-        """
-        Falls das Objekt bereits finalisiert wude -> invalid
-        """
-        # if (self.instance.finished):
-        #     return False
-
-        """
-        Falls Thesis-Documents finalisiert werden soll, setze all Felder 'required'
-        """
-
-        req = True if self.data.has_key('finalize') and self.data['finalize'] == 'true' else False
-
-        for key, val in self.fields.iteritems():
-            if not key in ['public', 'finished']:
-                val.required = req
-            else:
-                val.required = False
-
-        is_valid = super(FormThesisDocuments, self).is_valid()
-
-        """
-        Falls Thesis-Documents valid und finalize -> Thesis.finished
-        """
-
-        if is_valid and req:
-            # self.instance.documents_finished = True
-            self.instance.save()
-
-        return is_valid
-
-    class Meta:
-        model = Thesis
-        exclude = ['student', 'finished', 'sent_on', 'state', 'course', 'documents_finished']
-        fields = '__all__'
-        widgets = {
-            'report': ClearableFileInput(attrs={'accept': 'application/pdf'}),
-            'poster': ClearableFileInput(attrs={'accept': 'application/pdf'}),
-        }
-
-
 class FormThesisMentoringrequest(forms.ModelForm):
     def is_valid(self):
         req = True if self.data.has_key('finalize') else False
@@ -215,12 +119,6 @@ class FormThesisMentoringrequest(forms.ModelForm):
         widgets = {
             'task': forms.Textarea()
         }
-
-
-class FormStudent(forms.ModelForm):
-    class Meta:
-        model = Student
-        fields = '__all__'
 
 
 FormsetWorkCompany = forms.inlineformset_factory(Thesis, WorkCompany, fields=['address'], extra=1,
@@ -277,9 +175,6 @@ class FormMentoringTutor(forms.ModelForm):
         fields = '__all__'
 
 
-FormsetMentoringTutor2 = forms.inlineformset_factory(Mentoring, Tutor2ContactData, fields='__all__', extra=1)
-
-
 # Todo FormStudent alternative E-Mail anpassen
 class FormSettingsUser(forms.ModelForm):
     class Meta:
@@ -295,38 +190,6 @@ FormsetUserStudent = forms.inlineformset_factory(MentoringUser, Student,
                                                  fields='__all__',
                                                  extra=1, can_delete=False)
 FormsetStudentAddress = forms.inlineformset_factory(Student, Address, fields='__all__', extra=1, can_delete=False)
-
-
-class FormRegistration(forms.ModelForm):
-    class Meta:
-        model = Registration
-        fields = '__all__'
-        exclude = ['mentoring', 'permission_library_tutor', 'pdf_file', 'finished']
-
-
-class FormRegistrationExamination(forms.ModelForm):
-    class Meta:
-        model = ResponseExaminationBoard
-        fields = '__all__'
-        exclude = ['registration', 'finished']
-
-
-class FormColloquium(forms.ModelForm):
-    class Meta:
-        model = Colloquium
-        fields = '__all__'
-        exclude = ['mentoring']
-
-
-class FormMentoringReport(forms.ModelForm):
-    class Meta:
-        model = MentoringReport
-        fields = '__all__'
-        exclude = ['mentoring']
-
-
-FormsetReportItems = forms.inlineformset_factory(MentoringReport, MentoringReportItem, fields='__all__', extra=1,
-                                                 can_delete=True)
 
 
 class CommentForm(forms.ModelForm):
