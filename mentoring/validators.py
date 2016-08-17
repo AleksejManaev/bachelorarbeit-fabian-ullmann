ddcf6e3d707047dc724bdc6bff57a54bc7e4e7eb
levental <levental@fh-brandenburg.de>
2016-08-17 15:05:59 +0200

    PDF-Validator entfernt

--- mentoring/validators.py
+++ mentoring/validators.py
@@ -2,10 +2,6 @@ from django.conf import settings
 from django.core.exceptions import ValidationError
 
 
-def validate_pdf(value):
-    if not value.read(5) == '%PDF-':
-        raise ValidationError("This file is not in PDF format.")
-
 def validate_size(value):
     if not value.file.size <= getattr(settings, 'FILE_UPLOAD_MAX_SIZE'):
         raise ValidationError("This filesize is to big.")
