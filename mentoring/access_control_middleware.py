c96e405f9cf87b552d9ae7a0e21b13eaad8b6d83
levental <levental@fh-brandenburg.de>
2016-09-13 18:09:12 +0200

    Weiterleitung zur Startseite bei für den Nutzer nicht erlaubten URLs

--- mentoring/access_control_middleware.py
+++ mentoring/access_control_middleware.py
@@ -1,7 +1,8 @@
 import re
 
+from django.core.urlresolvers import reverse
 from django.db.models import Q
-from django.http import HttpResponseForbidden
+from django.http import HttpResponseRedirect
 
 from mentoring.models import AbstractWork, Comment
 
@@ -12,40 +13,40 @@ class AccessControlMiddleware(object):
 
         if re.fullmatch('/(student|tutor|comments)/.*', request_url):
             if not hasattr(request.user, 'portaluser'):
-                return HttpResponseForbidden()
+                return HttpResponseRedirect(reverse('index'))
 
             if request_url.startswith('/student'):
                 # Der Student darf auf student-Views zugreifen, der Tutor nicht
                 if hasattr(request.user.portaluser, 'student'):
                     return None
                 elif hasattr(request.user.portaluser, 'tutor'):
-                    return HttpResponseForbidden()
+                    return HttpResponseRedirect(reverse('index'))
             elif request_url.startswith('/tutor'):
                 # Der Student darf nicht auf tutor-Views zugreifen, der Tutor darf auf tutor-Views zugreifen, aber für einige Views wird eine zusätzliche Berechtigung verlangt
                 if hasattr(request.user.portaluser, 'student'):
-                    return HttpResponseForbidden()
+                    return HttpResponseRedirect(reverse('index'))
                 elif hasattr(request.user.portaluser, 'tutor'):
                     if request_url.startswith('/tutor/placementseminar'):
                         if request.user.portaluser.tutor.placement_responsible:
                             return None
                         else:
-                            return HttpResponseForbidden()
+                            return HttpResponseRedirect(reverse('index'))
                     elif re.fullmatch('/tutor/thesis.*seminar.*', request_url):
                         if request.user.portaluser.tutor.thesis_responsible:
                             return None
                         else:
-                            return HttpResponseForbidden()
+                            return HttpResponseRedirect(reverse('index'))
                     elif request_url.startswith('/tutor/posters'):
                         if request.user.portaluser.tutor.poster_responsible:
                             return None
                         else:
-                            return HttpResponseForbidden()
+                            return HttpResponseRedirect(reverse('index'))
                     elif re.fullmatch('/tutor/(placement|thesis)/.*', request_url):
                         # Der Tutor darf nur auf Praktika oder Abschlussarbeiten zugreifen, die ihm vom Studenten zur Betreuung freigegeben wurden
                         if AbstractWork.objects.filter(id=view_kwargs['pk'], tutor=request.user.portaluser.tutor, mentoring_requested=True).exists():
                             return None
                         else:
-                            return HttpResponseForbidden()
+                            return HttpResponseRedirect(reverse('index'))
                     else:
                         return None
             elif re.fullmatch('/comments/abstractwork/\d+', request_url):
@@ -53,12 +54,12 @@ class AccessControlMiddleware(object):
                 if AbstractWork.objects.filter(Q(id=view_kwargs['pk']), Q(student=request.user.portaluser) | Q(tutor=request.user.portaluser)).exists():
                     return None
                 else:
-                    return HttpResponseForbidden()
+                    return HttpResponseRedirect(reverse('index'))
             elif re.match('/comments/abstractwork/toggleprivacy', request_url):
                 # Nur der Ersteller des Kommentars darf die Sichtbarkeit des Kommentars ändern
                 if Comment.objects.filter(id=request.POST.get('id'), author=request.user.id).exists():
                     return None
                 else:
-                    return HttpResponseForbidden()
+                    return HttpResponseRedirect(reverse('index'))
         else:
             return None
